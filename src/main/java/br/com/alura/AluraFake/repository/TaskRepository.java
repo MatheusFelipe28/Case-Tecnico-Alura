8b5e3e329053435f71c4dbb0019c19d94843ba35
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-09-29 04:24:00 -0300

    Adicionando um List no repository de task

--- src/main/java/br/com/alura/AluraFake/repository/TaskRepository.java
+++ src/main/java/br/com/alura/AluraFake/repository/TaskRepository.java
@@ -1,8 +1,13 @@
 package br.com.alura.AluraFake.repository;
 
+import br.com.alura.AluraFake.entity.Course;
 import br.com.alura.AluraFake.entity.Task;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface TaskRepository extends JpaRepository<Task, Long> {
+
+    List<Task> findByCourseId(Long courseId);
 
 }
