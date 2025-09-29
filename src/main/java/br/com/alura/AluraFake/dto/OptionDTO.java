70b390966813f09ccefd75bd51dc02a1823ba2c5
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-09-29 04:22:16 -0300

    Criando DTOS da classe de options

---
+++ src/main/java/br/com/alura/AluraFake/dto/OptionDTO.java
@@ -0,0 +1,32 @@
+package br.com.alura.AluraFake.dto;
+
+
+public class OptionDTO {
+    private Long id;
+    private String text;
+    private boolean isCorrect;
+
+    public String getText() {
+        return text;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setText(String text) {
+        this.text = text;
+    }
+
+    public boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(boolean isCorrect) {
+        this.isCorrect = isCorrect;
+    }
+}
