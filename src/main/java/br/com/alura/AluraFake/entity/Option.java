371e04d238c140fd55ea031522e5922033a01ebd
Matheus Fernandes <matheusfelipegomesfernandes@gmail.com>
2025-09-29 04:21:55 -0300

    Criando classe de options

---
+++ src/main/java/br/com/alura/AluraFake/entity/Option.java
@@ -0,0 +1,49 @@
+package br.com.alura.AluraFake.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String option;
+
+    private boolean isCorrect;
+
+    public Option(Long id, String option, boolean isCorrect) {
+        this.id = id;
+        this.option = option;
+        this.isCorrect = isCorrect;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+
+    public boolean getIsCorrect() {
+        return isCorrect;
+    }
+
+    public void setIsCorrect(boolean isCorrect) {
+        isCorrect = isCorrect;
+    }
+}
+
